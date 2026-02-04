690d1eb15cab454ae8851f595489a861f5e47b36
sorumstylXjava <sorumstyl@gmail.com>
2026-02-04 17:38:25 +0700

    Update HomeScreen.kt

--- app/src/main/java/com/chargercontrol/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/chargercontrol/ui/screens/HomeScreen.kt
@@ -1,4 +1,4 @@
-package com.chargercontrol.fas.ui.screens
+package com.chargercontrol.ui.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,7 +10,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.chargercontrol.fas.utils.BatteryControl
+import com.chargercontrol.utils.BatteryControl
 
 @Composable
 fun HomeScreen() {
