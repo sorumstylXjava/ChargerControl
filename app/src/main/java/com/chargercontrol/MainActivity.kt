b0a8e33c8fc72b6295ad0a5782b92636ebca3aeb
sorumstylXjava <sorumstyl@gmail.com>
2026-02-04 17:19:30 +0700

    Update MainActivity for navigation and theming
    Refactored MainActivity to use ChargerControlTheme and added navigation.

--- app/src/main/java/com/chargercontrol/MainActivity.kt
+++ app/src/main/java/com/chargercontrol/MainActivity.kt
@@ -1,42 +1,46 @@
-package com.chargercontrol
+package com.chargercontrol.fas
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.chargercontrol.ui.theme.MyComposeApplicationTheme
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.chargercontrol.fas.ui.components.FloatingNavBar
+import com.chargercontrol.fas.ui.screens.*
+import com.chargercontrol.fas.ui.theme.ChargerControlTheme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyComposeApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+            ChargerControlTheme {
+                MainApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!", modifier = modifier)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyComposeApplicationTheme {
-        Greeting("Android")
+fun MainApp() {
+    val navController = rememberNavController()
+    Scaffold(
+        containerColor = Color(0xFF121212),
+        bottomBar = { FloatingNavBar(navController) }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "status",
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable("home") { HomeScreen() }
+            composable("status") { StatusScreen() }
+            composable("settings") { SettingsScreen() }
+        }
     }
 }
