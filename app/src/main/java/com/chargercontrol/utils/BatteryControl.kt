dcfe1839da2978e02c8b3235b692ccf794cb6070
sorumstylXjava <sorumstyl@gmail.com>
2026-02-04 17:25:36 +0700

    Create BatteryControl.kt

---
+++ app/src/main/java/com/chargercontrol/utils/BatteryControl.kt
@@ -0,0 +1,18 @@
+package com.chargercontrol.fas.utils
+
+import java.io.DataOutputStream
+
+object BatteryControl {
+    fun setChargingLimit(limit: Int): Boolean {
+        return try {
+            val process = Runtime.getRuntime().exec("su")
+            val os = DataOutputStream(process.outputStream)
+            os.writeBytes("echo $limit > /sys/class/power_supply/battery/charge_control_limit\n")
+            os.writeBytes("exit\n")
+            os.flush()
+            process.waitFor() == 0
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
